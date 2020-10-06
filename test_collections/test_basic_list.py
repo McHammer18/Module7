e9626bf0e426cfeedd50e0ad4fa2424b44e85f7b
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-06 18:50:06 -0500

    first test passes adding an exception test

--- test_collections/test_basic_list.py
+++ test_collections/test_basic_list.py
@@ -1,12 +1,17 @@
 import unittest
 from unittest.mock import patch
 from fun_with_collections import basic_list as basic
-
+from fun_with_collections import  basic_list_exception as basic_list
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.topic1.get_input', return_value='5')
+    @patch('fun_with_collections.basic_list.get_input', return_value='5')
     def test_make_list(self, input):
-        self.assertEqual(basic.make_list(), [5, 5, 5])
+        self.assertEqual(basic.make_list(), [5, 5, 5, 5, 5])
+
+    @patch('fun_with_collections.basic_list.get_input', return_value='ab')
+    def test_make_list_non_numeric(self, input):
+        with self.assertRaises(ValueError):
+            basic_list.make_list()
 
 if __name__ == '__main__':
     unittest.main()
