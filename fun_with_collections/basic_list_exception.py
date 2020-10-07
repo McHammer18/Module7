bead7d7fafdb39db72304fb748d3fff4328a941f
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-06 19:01:44 -0500

    Unit test for checking for exception all pass

--- fun_with_collections/basic_list_exception.py
+++ fun_with_collections/basic_list_exception.py
@@ -18,5 +18,8 @@ def get_input():
 
 
 if __name__ == '__main__':
-    test_list = make_list()
-    print(test_list)
+    try:
+        test_list = make_list()
+        print(test_list)
+    except ValueError:
+        print("You gave an invalid input please give numbers")
