2651f1dfed0754d038ef4d3862284f63b1630edf
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-11 16:01:16 -0500

    sort test passes

--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -13,10 +13,10 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_search_array_not_found(self):
-        self.assertEqual(array.search_array(self.intial_array, 36), -1)
+        self.assertEqual(array.search_array(self.initial_array, 36), -1)
 
     def test_sort_inorder(self):
-        self.assertEqual(array.sort_array(self.intial_array), self.expected_array)
+        self.assertEqual(array.sort_array(self.initial_array), self.expected_array)
 
 if __name__ == '__main__':
     unittest.main()
