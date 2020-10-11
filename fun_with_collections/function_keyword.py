fb8e3de5e214f758fa59a1fcc77eca118ac2fa3b
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-11 15:29:51 -0500

    final commit

--- fun_with_collections/function_keyword.py
+++ fun_with_collections/function_keyword.py
@@ -2,19 +2,21 @@
 Morgan Christensen
 
 Module7
-Program that has a function using arbitrary argument lits,
+Program that has a function using arbitrary argument lists,
 and keyword arguments to return a string
 """
 
 
 def average_scores(*args, **kwargs):
-    # Use *args for average calculation
-    for key, value in kwargs.items():
 
+    for key, value in kwargs.items():
+        average = (args[0] + args[1] + args[2] + args[3])/4
         print("%s == %s" % (key, value))
 
-    #return 'Resukts: name = {} {}, gpa = {}, major = {}'.format(kwargs['first_name'], kwargs['last_name'], , kwargs['major'])
+    return 'Results: name = {} {}, gpa = {}, major = {}'.format(kwargs['first_name'],  kwargs['last_name'], average, kwargs['major'])
 
 
 if __name__ == '__main__':
     print(average_scores(4, 3, 2, 4, first_name='Michelle', last_name='Ruse', major='World_domination'))
+    print(average_scores(5, 1, 6, 8, first_name='Austin', last_name='Powers', major='Super villian'))
+    print(average_scores(7, 2, 8, 6, first_name='Micheal', last_name='Myers', major='Hero'))
